016753d026a5c4828f78b30753663ce6a7b5150a
Tonny Nguyen <148969983+totodesigns@users.noreply.github.com>
2025-06-02 15:53:09 +0200

    search functionality for plp done

---
+++ hovedopgave/src/components/ToggleChip.vue
@@ -0,0 +1,35 @@
+<script setup>
+    defineProps({
+    modelValue: Boolean
+    })
+    defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div
+    class="chip"
+    :class="{ active: modelValue }"
+    @click="$emit('update:modelValue', !modelValue)"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.chip {
+    display: flex;
+    height: 32px;
+    width: fit-content;
+    padding: 0px 8px;
+    align-items: center;
+    gap: 4px;
+    border-radius: 4px;
+    background-color: var(--color-surface-white);
+    font-family: 'Fotext-Regular';
+}
+
+.chip.active {
+  background-color: var(--color-surface-default);
+  color: var(--color-text-white);
+}
+</style>
---
+++ hovedopgave/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCF0R2pY8Pbt-dN_zFk2e0-KiozQ8tgClM",
+  authDomain: "hovedopgave-c5c39.firebaseapp.com",
+  projectId: "hovedopgave-c5c39",
+  storageBucket: "hovedopgave-c5c39.firebasestorage.app",
+  messagingSenderId: "692133781916",
+  appId: "1:692133781916:web:a63559350af82160db6859"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const databaseURL = "https://hovedopgave-c5c39-default-rtdb.europe-west1.firebasedatabase.app/"
+const database = getDatabase(app, databaseURL);
+export { database };
